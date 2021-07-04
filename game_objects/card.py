7d3c161f9092c17edc9c39b8c70fb67649d9160c
Nate Brooks <nateonguitar@gmail.com>
2021-07-03 18:59:45 -0600

    Coloring card suits

--- game_objects/card.py
+++ game_objects/card.py
@@ -1,3 +1,4 @@
+import colorama
 from frolic import GameObject, Matrix, MatrixBorder
 
 class Card(GameObject):
@@ -8,9 +9,10 @@ class Card(GameObject):
             raise Exception('suit must be a string and must not be empty')
         self.value = value
         self.suit = suit
+        self.color = colorama.Fore.RED if suit == '♥' or suit == '♦' else colorama.Fore.GREEN
         matrix = Matrix.empty_sized(rows=4, columns=4, value=' ')
         border = MatrixBorder()
         matrix = matrix.with_border(border)
         matrix[1][1] = self.value
-        matrix[1][2] = self.suit
+        matrix[1][2] = f'{colorama.Style.BRIGHT}{self.color}{self.suit}{colorama.Style.RESET_ALL}'
         super().__init__(matrix=matrix)
